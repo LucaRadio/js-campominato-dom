fc2e2e95383e62699f98d2b14442d8761b66f384
LucaRadio <luca.1407@hotmail.it>
2022-11-10 17:26:37 +0100

    Complete Half Super-bonus :(

---
+++ js/main.js
@@ -0,0 +1,79 @@
+const playBtn = document.querySelector(".btn");
+
+const selectEl = document.querySelector("[name='rowCells']");
+
+const gridContainerEl = document.querySelector(".grid-container");
+
+let count = 1;
+
+function randomNumberGenerator(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+playBtn.addEventListener("click", function () {
+    gridContainerEl.innerHTML = ""
+    gridContainerEl.classList.remove("disabled")
+    let dimension;
+    const option = parseInt(selectEl.value);
+    if (option === 10) {
+        dimension = 100
+    } else if (option === 9) {
+        dimension = 81
+    } else if (option === 7) {
+        dimension = 49
+    }
+    cellsGenerator(dimension);
+
+
+})
+
+function cellsGenerator(dimension) {
+    let bombPosition = bombGenerator(dimension);
+    console.log(bombPosition);
+    for (let i = 0; i < dimension; i++) {
+        const monoCell = document.createElement("div");
+        monoCell.classList.add("cell");
+        monoCell.style.width = `calc(100% / ${Math.sqrt(dimension)})`;
+
+        monoCell.dataset.cellNumber = `${i + 1}`;
+
+
+        let cellNumber = parseInt(monoCell.dataset.cellNumber);
+        // if (bombPosition.includes(cellNumber)) {
+        //     monoCell.classList.add("mark")
+        // }
+        monoCell.addEventListener("click", function () {
+            this.classList.add("disabled")
+            if (bombPosition.includes(cellNumber)) {
+                this.classList.add("bg-danger")
+                alert("Hai Trovato una Bomba. Game over!")
+                gridContainerEl.classList.add("disabled")
+            } else {
+                this.classList.add("bg-primary")
+                count++;
+                if (count === dimension - 16) {
+                    alert("Hai Vinto! Complimenti!")
+                }
+            }
+        });
+
+        monoCell.removeEventListener("click", function () { });
+        gridContainerEl.append(monoCell);
+    }
+
+
+}
+
+function bombGenerator(dimension) {
+    let bombList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+
+    // while (bombList.length < 16) {
+    //     const bombPosition = randomNumberGenerator(1, dimension);
+
+    //     if (!bombList.includes(bombPosition)) {
+    //         bombList.push(bombPosition)
+    //     }
+    // }
+    return bombList;
+}
